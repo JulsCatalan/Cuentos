af6e6ef16b0e57593c92b03ac228296502671c16
JulsCatalan <julscatalan@gmail.com>
2024-02-12 12:35:40 -0600

    second commit

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ function handleScroll() {
     // Si la cantidad de scroll es mayor que 500, cambiar el color de fondo del navbar
     if (scrollTop > 400) {
         navbar.style.backgroundColor = 'rgba( 0,0,0, 0.8)';
-        navbar.sty
 
     } else {
         navbar.style.backgroundColor = '';
